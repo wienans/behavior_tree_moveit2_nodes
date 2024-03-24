9011f5bc2ad6078d47ba6f8d54062ec9dcc4e2a1
wienans <sven.wienand@outlook.com>
2024-03-24 15:46:44 +0100

    Proper Parallel Node to Plan

--- behavior_tree_moveit2_nodes/include/behavior_tree_moveit2_nodes/PlanToPose.hpp
+++ behavior_tree_moveit2_nodes/include/behavior_tree_moveit2_nodes/PlanToPose.hpp
@@ -3,13 +3,12 @@
 namespace bt_moveit2_nodes
 {
 using namespace BT;
-class PlanToPose : public SyncActionNode
+class PlanToPose : public StatefulActionNode
 {
 public:
   PlanToPose(const std::string & name, const NodeConfig & config)
-  :SyncActionNode(name, config)
+  :StatefulActionNode(name, config)
   {
-    plan_thread_ = std::make_shared<std::thread>(&PlanToPose::plan, this);
   }
 
   static PortsList providedPorts()
@@ -17,32 +16,58 @@ public:
     return {InputPort<geometry_msgs::msg::PoseStamped>("pose_stamped")};
   }
 
-
-  NodeStatus tick() override
+  NodeStatus onStart() override
   {
-    if (plan_thread_->joinable()) {
+    isRunning_ = true;
+    std::cout << "start" << std::endl;
+    plan_thread_ = std::make_shared<std::thread>(
+      std::bind(&PlanToPose::plan, this, std::ref(isRunning_))
+    );
+    return NodeStatus::RUNNING;
+  }
+  NodeStatus onRunning() override
+  {
+    if (isRunning_) {
       return NodeStatus::RUNNING;
-    } else {
-      return NodeStatus::SUCCESS;
     }
+    plan_thread_->join();
+    return NodeStatus::SUCCESS;
+  }
+
+  void onHalted() override
+  {
+    isRunning_ = false;
+    plan_thread_->detach();
   }
 
 private:
-  void plan()
+  void plan(std::atomic<bool> & isRunning)
   {
+    std::cout << "start plan" << std::endl;
     if (auto any_ptr = getLockedPortContent("pose_stamped")) {
+      std::cout << "lock" << std::endl;
       if (!any_ptr->empty()) {
         auto * pose_ptr = any_ptr->castPtr<geometry_msgs::msg::PoseStamped>();
 
-        std::cout << pose_ptr->pose.position.x << " " << pose_ptr->pose.position.y << " " <<
+        std::cout << "Test" << pose_ptr->pose.position.x << " " << pose_ptr->pose.position.y <<
+          " " <<
           pose_ptr->pose.position.z << std::endl;
+      } else {
+        std::cout << "empty" << std::endl;
       }
+    } else {
+      std::cout << "not locked" << std::endl;
     }
-    std::this_thread::sleep_for(std::chrono::seconds(10));
+    for (int i = 0; i < 5; i++) {
+      std::cout << i << std::endl;
+      std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
+    isRunning = false;
   }
 
 
   std::shared_ptr<std::thread> plan_thread_;
+  std::atomic<bool> isRunning_;
 };
 
 } // namespace name
